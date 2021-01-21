76a24e9bc8320a7a1d4d10234d4802ad1f2c17f5
peterbest7 <peterbest7@163.com>
2021-01-21 12:23:15 +0800

    my first time to submit assignment on github

---
+++ src/test/java/com/peter/gitgithub/GitGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.gitgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
