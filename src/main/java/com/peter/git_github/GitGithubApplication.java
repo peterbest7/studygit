1b79a7e34c94b7096b1f3d7469f63571be0f770e
peterbest7 <peterbest7@163.com>
2021-01-21 12:30:42 +0800

    hello github

---
+++ src/main/java/com/peter/git_github/GitGithubApplication.java
@@ -0,0 +1,13 @@
+package com.peter.git_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitGithubApplication.class, args);
+    }
+
+}
